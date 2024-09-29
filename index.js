71546af788bd3478057ebd3a19776bd902002bb6
Hector Castro <raidrex@hotmail.com>
2024-09-28 22:09:12 -0600

    config de knex y variables de entorno .env

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express() // inicializar express
 
 const PORT = process.env.PORT || 3000
 
-app.use(express.json()) //para que express entienda json
+app.use(express.json()) // para que express entienda json
 
-app.get('/', (req, res) => {  //las rutas de mi servidor
-    res.send('Hello World!')
+app.get('/', (req, res) => { // las rutas de mi servidor
+  res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server runing on http://localhost:${PORT}`)
-}) //puerto de mi servidor para escuchar las peticiones
+  console.log(`Server runing on http://localhost:${PORT}`)
+}) // puerto de mi servidor para escuchar las peticiones
