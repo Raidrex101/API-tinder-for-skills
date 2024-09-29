8144ed75a6b66b6812cd8e8e4a65e31179f3a7f8
Hector Castro <raidrex@hotmail.com>
2024-09-28 21:09:24 -0600

    se agrega puerto dinamico

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express') // importar express
 
 const app = express() // inicializar express
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.json()) //para que express entienda json
 
 app.get('/', (req, res) => {  //las rutas de mi servidor
     res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-    console.log('Server runing on http://localhost:3000')
+app.listen(PORT, () => {
+    console.log(`Server runing on http://localhost:${PORT}`)
 }) //puerto de mi servidor para escuchar las peticiones
