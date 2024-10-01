f30ba8ac2e14a34482afad6ca465277da63f98c8
Hector Castro <raidrex@hotmail.com>
2024-09-30 19:56:53 -0600

    ✨ peticion para crear un usuario

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express') // importar express
 const path = require('path')
 
+// importar las rutas de mis vistas
+const usersRoutes = require('./routes/userRoutes')
+
 const app = express() // inicializar express
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json()) // para que express entienda json
+// rutas definidas para la aplicacion
+app.use('/api/v1', usersRoutes)
 
 app.get('/', (req, res) => { // las rutas de mi servidor
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
